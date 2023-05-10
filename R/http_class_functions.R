a3709be71897fce8363dad8d1593fc6d7eb51b08
prei007 <peter.reimann@sydney.edu.au>
2023-05-10 21:08:19 +1000

    Changed so that data frames get returned for CONSTRUCT and DESCRIBE.

--- R/http_class_functions.R
+++ R/http_class_functions.R
@@ -126,11 +126,16 @@ ag_data = function(service, url,queryargs,body,returnType = NULL,cleanUp,convert
     return(content(resp))
 # new case: a construct query
   } else if (grepl("construct",tolower(queryargs$query))){
-    return(content(resp, "text"))
-# continue with old code; note that DESCRIBE needs also fixing.
+    parsed = jsonlite::fromJSON(content(resp,"text"),simplifyVector = TRUE)
+    # parsed is a m nx3 matrix of strings, with 3 columns for subject, predicate, and object.
+    # check if we have any results:
+    if(length(parsed[,1])==0) stop("Query did not return any results")
+    ret = as.data.frame(parsed,stringsAsFactors = FALSE)
+    colnames(ret) = c("subject", "predicate", "object")
+# continue with old code;
   } else if(grepl("describe",tolower(queryargs$query))){
 # changing what's shown about a DESCRIBE query
-    return(content(resp, "text"))
+#  return(content(resp, "text"))
       # parsed = jsonlite::fromJSON(content(resp,"text"),simplifyVector = TRUE)
       # if(is.integer(mean(unlist(lapply(lapply(parsed,as.list),length))))){
       #   ret = do.call(rbind,parsed)
@@ -140,6 +145,10 @@ ag_data = function(service, url,queryargs,body,returnType = NULL,cleanUp,convert
       #   ret = parsed
       #   cleanUp = FALSE
       # }
+    parsed = jsonlite::fromJSON(content(resp,"text"),simplifyVector = TRUE)
+    if(length(parsed[,1])==0) stop("Query did not return any results")
+    ret = as.data.frame(parsed,stringsAsFactors = FALSE)
+    colnames(ret) = c("subject", "predicate", "object")
 # continuing with old code from here on
   } else if(http_type(resp) == "application/json"){
       parsed = jsonlite::fromJSON(content(resp,"text"),simplifyVector = TRUE)
